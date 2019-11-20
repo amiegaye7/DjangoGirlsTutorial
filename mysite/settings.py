b57c04e6d1f2cfbc63d532e7b77128dfdc5cc59f
amiegaye <amiegaye7@gmail.com>
2019-11-20 19:06:21 +0000

    Initial Update

--- mysite/settings.py
+++ mysite/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'k1s6(o%7y4m$&o)bl^5uq$2q@meeqejdsn_#kxdhh%2pa624w2'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["127.0.0.1"]
+ALLOWED_HOSTS = ["127.0.0.1", ".pythonanywhere.com"]
 
 
 # Application definition
